a90742f155f0a3938238c15c7b5606059bbf39f0
Spratch <contact@josephclenet.fr>
2025-03-09 16:09:01 +0100

    feat(buttons): better animations

--- components/button.tsx
+++ components/button.tsx
@@ -23,14 +23,14 @@ export default function ButtonLambda({ onPress, label, Icon }: Props) {
     <Button
       onPress={handleClick}
       aria-label={label}
-      className="flex items-center justify-center rounded-full bg-white text-black transition text-nowrap gap-3.5 select-none py-2 px-4 text-lg cursor-pointer hover:scale-103 motion-safe:hover:rotate-3 focus-visible:outline-2 outline-offset-2 outline-yellow-700"
+      className="flex items-center justify-center rounded-full bg-white text-black transition-all text-nowrap gap-3.5 select-none py-2 px-4 text-lg cursor-pointer hover:scale-103 motion-safe:hover:rotate-3 focus-visible:outline-2 outline-offset-2 outline-yellow-700 pressed:scale-95 pressed:shadow shadow-yellow-200 hover:shadow-md"
     >
       <Icon
         size={20}
         className={
           isClicked
             ? Icon.displayName === "Wand"
-              ? "animate-wand"
+              ? "animate-wand origin-left"
               : Icon.displayName === "RefreshCcw"
                 ? "animate-invert"
                 : ""
