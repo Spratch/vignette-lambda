52539c3905d80cfb3d8d2bb8833c5a464e3e2f2c
Spratch <contact@josephclenet.fr>
2025-09-07 14:21:05 +0200

    feat(thumbnail): text animation

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -44,47 +44,54 @@ export default function Thumbnail({ names, images, time }: Props) {
             className="absolute top-0 bottom-5 -left-5 h-full w-8/12 sm:w-2xs object-cover object-top drop-shadow-2xl grayscale contrast-125 brightness-90 bg-gradient-to-r from-black via-black/75 to-transparent"
           />
         )}
+        <div
+          className="absolute inset-0"
+          style={{
+            backdropFilter: "blur(20px)",
+            maskImage:
+              "radial-gradient(circle, rgba(0,0,0,0) 30%, rgba(0,0,0,1) 100%)"
+          }}
+        ></div>
+        <div className="flex justify-center pb-1.5 sm:pb-3 pt-16 sm:pt-20 px-2 sm:px-6 absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent">
+          <motion.p
+            key={names.background + names.foreground}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 10, opacity: 0 }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
+            className="uppercase text-sm sm:text-lg text-center leading-tight"
+          >
+            {names.background !== "Toi" ? "Comprendre" : "Te comprendre"}{" "}
+            <span
+              className="font-medium text-nowrap"
+              style={{ textShadow: "0 0 4px gold" }}
+            >
+              {names.background}
+            </span>
+            &nbsp;grâce&nbsp;
+            {names.foreground.startsWith("Les ")
+              ? "aux"
+              : names.foreground.startsWith("Le ")
+                ? "au"
+                : "à"}{" "}
+            <br />
+            <span
+              className="pt-0.5 inline-block font-bold text-2xl sm:text-4xl tracking-wide leading-none"
+              style={{ textShadow: "0 0 6px gold" }}
+            >
+              {names.foreground.startsWith("Le ") ||
+              names.foreground.startsWith("Les ")
+                ? names.foreground.slice(3)
+                : names.foreground}
+            </span>
+          </motion.p>
+        </div>
+        <div
+          className={`${roboto.className} absolute right-2 bottom-2 px-1 py-px bg-white/20 text-2xs sm:text-xs tabular-nums rounded-sm  backdrop-blur-xs font-medium opacity-65 sm:opacity-100`}
+        >
+          {time}
+        </div>
       </AnimatePresence>
-      <div
-        className="absolute inset-0"
-        style={{
-          backdropFilter: "blur(20px)",
-          maskImage:
-            "radial-gradient(circle, rgba(0,0,0,0) 30%, rgba(0,0,0,1) 100%)"
-        }}
-      ></div>
-      <div className="flex justify-center pb-1.5 sm:pb-3 pt-16 sm:pt-20 px-2 sm:px-6 absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent">
-        <p className="uppercase text-sm sm:text-lg text-center leading-tight">
-          {names.background !== "Toi" ? "Comprendre" : "Te comprendre"}{" "}
-          <span
-            className="font-medium text-nowrap"
-            style={{ textShadow: "0 0 4px gold" }}
-          >
-            {names.background}
-          </span>
-          &nbsp;grâce&nbsp;
-          {names.foreground.startsWith("Les ")
-            ? "aux"
-            : names.foreground.startsWith("Le ")
-              ? "au"
-              : "à"}{" "}
-          <br />
-          <span
-            className="pt-0.5 inline-block font-bold text-2xl sm:text-4xl tracking-wide leading-none"
-            style={{ textShadow: "0 0 6px gold" }}
-          >
-            {names.foreground.startsWith("Le ") ||
-            names.foreground.startsWith("Les ")
-              ? names.foreground.slice(3)
-              : names.foreground}
-          </span>
-        </p>
-      </div>
-      <div
-        className={`${roboto.className} absolute right-2 bottom-2 px-1 py-px bg-white/20 text-2xs sm:text-xs tabular-nums rounded-sm  backdrop-blur-xs font-medium opacity-65 sm:opacity-100`}
-      >
-        {time}
-      </div>
     </div>
   );
 }
